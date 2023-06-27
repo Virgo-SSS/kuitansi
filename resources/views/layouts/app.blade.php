41bfa7b14645fba102a7507692264bcfebd05b72
virgo <virgostevanus27@gmail.com>
2023-06-28 02:45:51 +0700

    add layout admin, fix dark theme bug, add component for header and sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html :class="{ 'theme-dark': dark }" x-data="data()" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,34 +12,28 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/tailwind.output.css'])
+        
         <!-- Styles -->
-        @livewireStyles
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css"/>
     </head>
     <body class="font-sans antialiased">
         <x-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+        <!-- Page Content -->
+        <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
+            <x-desktop-sidebar></x-desktop-sidebar>
+            <x-mobile-sidebar></x-mobile-sidebar>
+            <div class="flex flex-col flex-1 w-full">
+                <main class="h-full overflow-y-auto">
+                    <x-header></x-header>
+                    {{ $slot }}
+                </main>
+            </div>
         </div>
-
         @stack('modals')
-
-        @livewireScripts
+        <script src="{{ asset('assets/js/init-alpine.js') }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer ></script>
+        <script src="{{ asset('assets/js/charts-lines.js') }}" defer></script>
+        <script src="{{ asset('assets/js/charts-pie.js') }}" defer></script>
     </body>
 </html>
