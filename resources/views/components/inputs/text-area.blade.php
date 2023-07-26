d625a5a59a6b2a2f06db6df36311315e86dbbb39
virgo <virgostevanus27@gmail.com>
2023-07-27 00:42:39 +0700

    fix container width

--- resources/views/components/inputs/text-area.blade.php
+++ resources/views/components/inputs/text-area.blade.php
@@ -1,4 +1,3 @@
 <textarea rows="3" {{ $attributes->merge(['class' => "block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-textarea
-focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"]) }}>
-    {{ $slot }}
+focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"]) }}>{{ $slot }}
 </textarea>
