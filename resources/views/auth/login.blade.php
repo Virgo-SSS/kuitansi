efd2848571e7ca3fb54a2ae8a4e30aaf140caa35
virgo <virgostevanus27@gmail.com>
2023-09-08 17:48:07 +0700

    fix wrong components name at login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,6 @@
             <x-authentication-card-logo />
         </x-slot>
 
-        <x-validation-errors class="mb-4" />
-
         @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
                 {{ session('status') }}
@@ -17,24 +15,24 @@
 
             <div>
                 <x-label for="uuid" value="{{ __('UUID') }}" />
-                <x-inputs.default id="uuid" class="block mt-1 w-full" type="text" name="uuid" :value="old('uuid')" required autofocus autocomplete="uuid" />
+                <x-inputs.text id="uuid" name="uuid" :value="old('uuid')" required autofocus autocomplete="uuid" />
+                @error('uuid')
+                <x-message.error>{{ $message }}</x-message.error>
+                @enderror
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-inputs.default id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-inputs.password id="password" name="password" required autocomplete="current-password" />
+                @error('password')
+                <x-message.error>{{ $message }}</x-message.error>
+                @enderror
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
                 <x-buttons.submit class="ml-4">
                     {{ __('Log in') }}
-                </x-button>
+                </x-buttons.submit>
             </div>
         </form>
     </x-authentication-card>
