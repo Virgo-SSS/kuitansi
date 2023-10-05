6fdc04a993e2e59491373c4e1a249d272f2b61ba
virgo <virgostevanus27@gmail.com>
2023-10-05 10:55:17 +0700

    fix wrong path logo at login

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ route('dashboard') }}" class="flex justify-center">
-    <img src="{{ asset('image/logo.png') }}" alt="" {{ $attributes->class(["w-18 h-16"]) }}>
+    <img src="{{ asset(config('receipt.logo')) }}" alt="" {{ $attributes->class(["w-18 h-16"]) }}>
 </a>
