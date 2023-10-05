b0c3a7b2f050eddf242b8392441bc4f15bc54151
virgo <virgostevanus27@gmail.com>
2023-10-05 21:05:01 +0700

    change company address

--- config/receipt.php
+++ config/receipt.php
@@ -4,7 +4,7 @@ return [
     'acceptance_receipt_code' => 'ABP-T',
     'payment_receipt_code' => 'ABP-B',
     'company_name' => 'PT. Adi Bintan Permata',
-    'company_address' => 'KOMP.RUKO INTI BATAM I/3A,SUNGAI PANAS,BATAM KOTA',
+    'company_address' => 'Ruko Trikarsa Equalita Blok B no 14-15 Batam',
     'logo' => 'assets/image/logo.png',
 
 ];
